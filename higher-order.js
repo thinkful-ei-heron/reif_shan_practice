1c0ecada5a07547983321b1151a693e7482de0be
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-17 13:33:23 -0600

    turtle steps

--- higher-order.js
+++ higher-order.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 // hello goodbye
 function repeat(fn, n) {
   for (let i = 0; i < n; i++) {
@@ -6,10 +6,10 @@ function repeat(fn, n) {
   }
 }
 function hello() {
-  console.log('Hello world');
+  console.log("Hello world");
 }
 function goodbye() {
-  console.log('Goodbye world');
+  console.log("Goodbye world");
 }
 
 // repeat(hello, 5);
@@ -27,14 +27,14 @@ function filter(arr, fn) {
 
 // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
 // First we setup an array of strings we plan to filter:
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+const myNames = ["Rich", "Joe", "Bhaumik", "Ray"];
 
 // We use your `filter` function here, capturing a new array into `filteredNames`
 // comprised of names that only begin with 'R'
 const filteredNames = filter(myNames, function(name) {
   // This is known as a "predicate function" - it's a function that
   // only returns a boolean
-  return name[0] === 'R';
+  return name[0] === "R";
 });
 
 // console.log(filteredNames); // => ['Rich', 'Ray']
@@ -47,9 +47,9 @@ function hazardWarningCreator(typeOfWarning) {
 
   return function(location) {
     warningCounter += 1;
-    let times = 'time';
+    let times = "time";
     if (warningCounter > 1) {
-      times = 'times';
+      times = "times";
     }
     console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
     console.log(
@@ -58,7 +58,24 @@ function hazardWarningCreator(typeOfWarning) {
   };
 }
 
-const warning = hazardWarningCreator('Rocks on the Road');
-warning('Dallas');
-warning('St Pete');
-warning('Denver');
+// const warning = hazardWarningCreator('Rocks on the Road');
+// warning('Dallas');
+// warning('St Pete');
+// warning('Denver');
+
+function turtleSteps(arr) {
+  let newArray = arr.filter(item => item[0] >= 0 && item[1] >= 0);
+
+  let numSteps = newArray.map(step => step[0] + step[1]);
+  let i;
+  let totalSteps = numSteps.forEach((steps, i) => {
+    let stepcounter = "steps";
+    if (steps <= 1) {
+      stepcounter = "step";
+    }
+    i += 1;
+    console.log(`Movement #${i}: ${steps} ${stepcounter}`);
+  });
+}
+
+turtleSteps([[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]]);
